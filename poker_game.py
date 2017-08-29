e3c7a22dcbea32eed240ddcacd6e036fa85f968d
msa190 <mauricio98sousa@gmail.com>
2017-08-29 20:43:36 -0300

    Update poker_game.py
    Desfiz o último merge por que ficou complicada a implementação da a montagem do baralho. Em compensação, criei dois métodos de entrada para Carta(a,b), um em que a e b podem ser string elementos de naipes e numeros, e outro em que a e b são inteiros contidos nos intervalos entre zero e comprimento das listas números ou naipes, para ser mais fácil chamar a lista por apenas um número, quando necessário. Como no momento não tenho condições de compilar e ver se está prestando, vou deixar como uma new branch e se funcionar, eu dou merge.

--- poker_game.py
+++ poker_game.py
@@ -1,10 +1,16 @@
-naipes=['ouros', 'paus','copas','espadas']
 numeros = ['As','2','3','4','5','6','7','8','9','10','J','Q','K']
+naipes=['ouros', 'paus','copas','espadas']
+
 class Carta:
 	def __init__(self, a, b):
-		self.numero = numeros[a]		
-		self.naipe = naipes[b]
-
+		if(a in numeros and b in naipes ):
+			self.numero = a		
+			self.naipe = b
+		if(a in range(len(numeros)) and b in range(len(naipes))):
+			self.numero = numeros[a]
+			self.naipes = naipes[b]
+		else:
+			raise
 class Mao:
 	def __init__(self,a):
 		numero_de_cartas = a
@@ -12,8 +18,9 @@ class Mao:
 	def mostrar():
 		for i in self.cartas:
 			print i.numero+'de'+ i.naipe
+			
 baralho = mao(52)
-for k,i in enumerate(numeros):
+for i in numeros:
 	for j in naipes:
 		baralho.cartas[k] = Carta(i,j)
 baralho.mostrar()
