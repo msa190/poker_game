e1bba9df50ae80178f03a1642ada905b718f38f8
msa190 <mauricio98sousa@gmail.com>
2017-08-29 20:16:58 -0300

    Update poker_game.py

--- poker_game.py
+++ poker_game.py
@@ -1,19 +1,17 @@
+naipes=['ouros', 'paus','copas','espadas']
+numeros = ['As','2','3','4','5','6','7','8','9','10','J','Q','K']
 class Carta:
 	def __init__(self, a, b):
 		self.numero = a		
 		self.naipe = b
 
-class mao:
+class Mao:
 	cartas = [0.0 for i in range(51)]
 	def __init__(self,a):
 		numero_de_cartas = a	
-		def mostrar():
-			for i in self.cartas:
-				print i.numero+'de'+ i.naipe
-
-naipes=['ouros', 'paus','copas','espadas']
-numeros = ['As','2','3','4','5','6','7','8','9','10','J','Q','K']
-
+	def mostrar():
+		for i in self.cartas:
+			print i.numero+'de'+ i.naipe
 baralho = mao(52)
 for k,i in enumerate(numeros):
 	for j in naipes:
